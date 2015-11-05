70fb973a2656f75302afd41f7dbf01136c1ec5cd
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2015-11-05 17:30:55 +1300

    form for guessing now working! yay.

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,11 +2,13 @@ class GuessesController < ApplicationController
   def create
     game = Game.find(params[:game_id])
     @guess = game.guesses.new(guess_params)
-    redirect_to game_path(game)
+    if @guess.save!
+      redirect_to game_path(game)
+    end
   end
 
 
   def guess_params
-    params.require(:guess).permit(:letter)
+    params.require(:guess).permit(:letter, :game_id)
   end
 end
